f8a8aeac455a9f189943cd956b3d989bb85710c9
Ser11y <99765361+Ser11y@users.noreply.github.com>
2022-02-16 19:04:45 +0300

    Обнова с амонгасиками
    Короче +2 команды вроде, да, ну и всё

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
-from colorama import Fore, init
 import discord
 from discord.ext import *
 from discord.ext import commands
 client = commands.Bot( command_prefix = '+')
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(942836408916275290)
+    print('Залогался!(Good 0x0000000001)')
+    await channel.send('{0.user.mention} залогинился в сеть, хуй'.format(client))
+    await channel.send('@Antonvel#2462 , ты лох'.format(client))
+    print('Я отправил сообщение Антону(good 0x0000000011)')
 
 @client.command(pass_context = True)
 @commands.has_permissions(administrator = True)
@@ -22,15 +29,35 @@ async def sclear(ctx, amount: int):
     await ctx.channel.purge(limit = amount)
 
 @client.command(pass_context=True)
+@commands.has_permissions( ban_members=True )
+async def nya(ctx):
+    await ctx.author.send('~uwu')
+    await ctx.author.send(':heart:')
+    await ctx.author.send('~uwu')
+    await ctx.author.send('~uwu')
+    await ctx.author.send('~uwu')
+
+@client.command(pass_context=True)
+async def kick(ctx, user: discord.Member,*,reason = 'Kicked by operator'):
+    await user.kick(reason=reason)
+    await ctx.channel.send(embed = discord.Embed(title = 'Кик', description = f'☑️ {user}, был выгонен', color = 0x11806a))
+
+
+
+@client.command(pass_context=True)
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, user: discord.Member,*,reason = 'Banned by operator'):
     await user.ban(reason=reason)
     await ctx.channel.send(f'{user} забанен по причине' + reason)
-
 @ban.error
 async def ban_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.channel.send(embed = discord.Embed(title = 'Ошибка!', description = f':no_entry_sign: {ctx.author.name}, ты полнйший лох!', color = 0x11806a))
+        await ctx.channel.send(embed = discord.Embed(title = 'Ошибка!', description = f':no_entry_sign: {ctx.author.name}, введи правильный аргумент!', color = 0x11806a))
+
+@kick.error
+async def kick_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.channel.send(embed = discord.Embed(title = 'Ошибка!', description = f':no_entry_sign: {ctx.author.name}, введи правильный аргумент!', color = 0x11806a))    
 
 @clear.error
 async def clear_error(ctx, error):
@@ -38,4 +65,4 @@ async def clear_error(ctx, error):
         await ctx.channel.send(embed = discord.Embed(title = 'Ошибка!', description = f':no_entry_sign: {ctx.author.name}, обязательно введите аргумент!', color = 0x11806a))
         
 #запуск
-client.run( '' )
+client.run( 'token' )
